5243ad51ddaef6f83c2dc90db5b2764996477e4a
James Boby Vempala <jvempala1304@conestogac.on.ca>
2024-04-15 16:03:26 -0400

    Integrated get car to frontend.Fixed bugs with add car form. Partially integrated create car API to frontend.

--- src/public/js/add-car.js
+++ src/public/js/add-car.js
@@ -3,48 +3,69 @@
 let carImage = null;
 
 $(async () => {
-	console.log("Add car script file");
+  console.log("Add car script file");
 
-	const dealersResponse = await axios.get("/dealer");
-	const dealers = dealersResponse.data;
+  const dealersResponse = await axios.get("/dealer");
+  const dealers = dealersResponse.data;
 
-	const dealerEle = $("#dealer");
-	dealers.forEach(dealer => {
-		dealerEle.append(`<option value=${dealer.id}>${dealer.name}</option>`);
-	});
+  const dealerEle = $("#dealer");
+  dealers.forEach((dealer) => {
+    dealerEle.append(`<option value=${dealer.id}>${dealer.name}</option>`);
+  });
 
-	listenForImageUpload();
+  listenForImageUpload();
 
-	$("#addCar").on("submit", (event) => {
-		event.preventDefault();
+  $("#addCar").on("submit", (event) => {
+    event.preventDefault();
 
-		console.log("dealer id :: car image :", $("#dealer").val(), carImage);
-	});
+    const carData = {
+      image: carImage,
+      make: $("#make").val(),
+      model: $("#model").val(),
+      year: $("#year").val(),
+      price: $("#price").val(),
+      mileage: $("#mileage").val(),
+      color: $("#color").val(),
+      condition: $("#condition").val(),
+      dealer: $("#dealer").val(),
+    };
+
+    axios
+      .post("/car/", carData)
+      .then((response) => {
+        console.log("Response:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    console.log("dealer id :: car image :", $("#dealer").val(), carImage);
+  });
 });
 
 function listenForImageUpload() {
-	$("#imageUpload").on("change", function (event) {
-		const file = event.target.files[0];
-		const reader = new FileReader();
+  $("#imageUpload").on("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-		reader.onload = function () {
-			carImage = reader.result;
-			$("#uploadedImage").attr("src", carImage);
-			$("#uploadContainer").css("display", "none");
-			$("#imageContainer").css("display", "block");
+    reader.onload = function () {
+      carImage = reader.result;
+      $("#uploadedImage").attr("src", carImage);
+      $("#uploadContainer").css("display", "none");
+      $("#imageContainer").css("display", "block");
 
-			console.log("carImage", carImage);
-		};
+      console.log("carImage", carImage);
+    };
 
-		reader.readAsDataURL(file);
-	});
+    reader.readAsDataURL(file);
+  });
 
-	$("#removeImg").on("click", function () {
-		carImage = null;
+  $("#removeImg").on("click", function () {
+    carImage = null;
 
-		$("#uploadContainer").css("display", "block");
-		$("#imageContainer").css("display", "none");
+    $("#uploadContainer").css("display", "block");
+    $("#imageContainer").css("display", "none");
 
-		console.log("carImage : remove ::", carImage);
-	});
+    console.log("carImage : remove ::", carImage);
+  });
 }
--- src/views/controllers/page.controller.js
+++ src/views/controllers/page.controller.js
@@ -1,29 +1,31 @@
 const express = require("express");
-
+const Cars = require("../../cars/repositories/car.repository");
 const router = express.Router();
 
 router.get("/", async (request, response, next) => {
-	try {
-		return response.render("index");
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const carRepository = Cars.CarRepository();
+    const cars = carRepository.findAll();
+    return response.render("index", { cars });
+  } catch (error) {
+    throw error;
+  }
 });
 
 router.get("/add-car", async (request, response, next) => {
-	try {
-		return response.render("add-car");
-	} catch (error) {
-		throw error;
-	}
+  try {
+    return response.render("add-car");
+  } catch (error) {
+    throw error;
+  }
 });
 
 router.get("/edit-car", async (request, response, next) => {
-	try {
-		return response.render("edit-car");
-	} catch (error) {
-		throw error;
-	}
+  try {
+    return response.render("edit-car");
+  } catch (error) {
+    throw error;
+  }
 });
 
 module.exports = router;
